46a177b03a6c259200b91ab53c7f84dc55a9c8a6
kimhyeok <kimmang940715@naver.com>
2024-04-17 18:06:41 +0900

    feat : 스페이스바 점프 게이지 모으기기능 구현

--- JumpRabbit/Assets/Scripts/Player.cs
+++ JumpRabbit/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigid = null;
     [SerializeField] protected float jumpPower = 1f;
+    [SerializeField] protected float currentJumpPower = 1f;
     public void Init()
     {
         
@@ -15,7 +16,21 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            _rigid.AddForce(Vector2.one * 0.5f * jumpPower);
+            
         }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            currentJumpPower += jumpPower;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _rigid.AddForce(Vector2.one * 0.5f * currentJumpPower);
+            currentJumpPower = 0f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _rigid.velocity = Vector2.zero;
     }
 }
